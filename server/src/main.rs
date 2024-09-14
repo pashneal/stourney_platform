7eefad20a4c48208b0a6be2d4522ac0b9d2496c1
Neal Powell <pashneal@gmail.com>
2024-09-13 22:35:32 -0400

    feat(wip): tinkering with sqlite and sqlx

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,83 @@
-use sqlx::sqlite::SqliteConnectOptions;
-use sqlx::SqlitePool;
+use sqlx::sqlite::{SqlitePoolOptions, SqlitePool};
+use futures::TryStreamExt;
+use sqlx::Row;
+
+/// Note: this uses sqlx compile time checker
+/// to ensure that the queries are correct
+/// be sure to run sqlx prepare if strange errors occur
+/// also add your DATABASE_URL is set in the .env file
+
 //#[async_std::main] // Requires the `attributes` feature of `async-std`
 #[tokio::main]
 // or #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
 
+    let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await?;
 
-    let pool = SqlitePool::connect_with(
-        SqliteConnectOptions::new()
-            .filename("test.db")
-            .create_if_missing(true)
-    );
 
-    let pool = pool.await?;
+    let email = "pashneal@gmail.com";
+    let mut rows = sqlx::query!("SELECT email FROM users WHERE email = ?", email)
+        .fetch(&pool);
 
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool).await?;
+    while let Some(row) = rows.try_next().await? {
+        print!("row: {:?}", row);
+    }
 
-    println!("Row: {:?}", row);
-    assert_eq!(row.0, 150);
 
+
+    Ok(())
+}
+
+/// The function that is called when the database is started
+/// responsible for setting up the database to get high performance
+/// speeds
+pub async fn on_start_up(pool: &SqlitePool) {
+    // Set option to use write ahead logging 
+    // which means multiple concurrent readers even
+    // during open write transactions
+    sqlx::query("PRAGMA journal_mode = WAL")
+        .execute(pool)
+        .await
+        .expect("Failed to set journal mode to WAL");
+
+    // Since we are using WAL mode, we
+    // don't need synchronous writes as WAL guarantees
+    // consistency in synchronous = normal mode
+    sqlx::query("PRAGMA synchronous = NORMAL")
+        .execute(pool)
+        .await
+        .expect("Failed to set synchronous mode to NORMAL");
+
+    // Store temporary tables and files in memory,
+    // we will lose data if the database is closed but 
+    // this is a trade off for speed
+    sqlx::query("PRAGMA temp_store = MEMORY")
+        .execute(pool)
+        .await
+        .expect("Failed to set temp store to MEMORY");
+    
+
+    // Use memory mapped I/O for reading and writing
+    // as it can be faster than normal I/O
+    // note this has implications for I/O errors on sqlite
+    sqlx::query("PRAGMA mmap_size = 30_000_000_000")
+        .execute(pool)
+        .await
+        .expect("Failed to set mmap size to 256MB");
+}
+
+
+pub async fn init_db() -> Result<(), sqlx::Error>{
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+    on_start_up(&pool).await;
     Ok(())
 }
